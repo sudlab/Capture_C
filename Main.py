7c79957a1b017340c22e5fc4e9ac62060a3ece14
Jaime Alvarez Benayas <jaime.alvarez.benayas@gmail.com>
2015-11-28 12:59:22 +0000

    Working branch to review

--- Main.py
+++ Main.py
@@ -4,6 +4,7 @@ import PipelineCaptureC as Capt_C
 if __name__ == "__main__":
     print("Before settrace")
     pydevd.settrace("86.177.59.46")
+    pydevd.
     print("After settrace")
     Capt_C.countInteractions('/fastdata/mbp15ja/capturec-pilot/capture_c_mb1imsbams/dedupped.dir/Jurkat-MERGE-R1.by_name.bam', 
                               '/fastdata/mbp15ja/capturec-pilot/capture_c_mb1imsbams/digest.dir/fragments.bed.gz',
