0f1b641b483509b9fb7bad7daf613b8d5f2c9fe4
Jaime A. Benayas <jaime.alvarez.benayas@gmail.com>
2015-12-03 22:11:54 +0000

    When creating new_bed in a change of chromosome (bed), new_bed end equals length of last chr

--- Main.py
+++ Main.py
@@ -1,15 +1,13 @@
-import pydevd
 import PipelineCaptureC as Capt_C
 
 if __name__ == "__main__":
-    print("Before settrace")
-    pydevd.settrace("86.177.59.46")
-    pydevd.
-    print("After settrace")
-    Capt_C.countInteractions('/fastdata/mbp15ja/capturec-pilot/capture_c_mb1imsbams/dedupped.dir/Jurkat-MERGE-R1.by_name.bam', 
-                              '/fastdata/mbp15ja/capturec-pilot/capture_c_mb1imsbams/digest.dir/fragments.bed.gz',
-                              '/fastdata/mbp15ja/capturec-pilot/capture_c_mb1imsbams/probe_fragments.bed.gz',
-                              '/fastdata/mbp15ja/capturec-pilot/capture_c_mb1imsbams/test_output1.txt',
-                              '/fastdata/mbp15ja/capturec-pilot/capture_c_mb1imsbams/test_output2.txt')
     
-    pydevd.settrace("86.177.59.46")
+    Capt_C.sites2fragments('/fastdata/mbp15ja/capturec-pilot/capture_c_sudlab_pe_raw/digest.dir/digest.bed.gz',
+                           '/shared/sudlab1/General/annotations/hg19_ensembl75/contigs.tsv',
+                           '/fastdata/mbp15ja/capturec-pilot/out_fragments.bed.gz')
+#     Capt_C.countInteractions('/fastdata/mbp15ja/capturec-pilot/capture_c_sudlab_pe_raw/dedupped.dir/IGF0003828.bwa.by_name.bam', 
+#                               '/fastdata/mbp15ja/capturec-pilot/capture_c_sudlab_pe_raw/digest.dir/fragments.bed.gz',
+#                               '/fastdata/mbp15ja/capturec-pilot/capture_c_sudlab_pe_raw/probe_fragments.bed.gz',
+#                               '/fastdata/mbp15ja/capturec-pilot/capture_c_sudlab_pe_raw/test_output1.txt',
+#                               '/fastdata/mbp15ja/capturec-pilot/capture_c_sudlab_pe_raw/test_output2.txt')
+#     
